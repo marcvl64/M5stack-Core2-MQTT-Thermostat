73819f1d0fcb7d9397d77098c3030d312a2eb96e
Marc <marc@Administrators-Mac-Pro-2.local>
2021-03-22 15:14:45 -0700

    Added initial comments

--- Thermostat.py
+++ Thermostat.py
@@ -1,3 +1,8 @@
+# Smart thermostat, built for M5Stack Core2, using MQTT to communicate with relays.
+# Integrated with Home Assistant (through MQTT). Autodiscovery of ENV sensor HVAC platform
+# Requires M5Stack Core2 and ENVII sensor
+# Work in progress. Non-functional.
+
 from m5stack import *
 from m5stack_ui import *
 from uiflow import *
